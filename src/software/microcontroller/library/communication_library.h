771f7af976c6be79eb4143214061325e98914e65
dilham1108 <73468701+dilham1108@users.noreply.github.com>
2024-07-13 15:22:14 +0700

    Create communication_library.h

---
+++ src/software/microcontroller/library/communication_library.h
@@ -0,0 +1,56 @@
+#ifndef COMMUNICATION_LIBRARY_H
+#define COMMUNICATION_LIBRARY_H
+
+#include <WiFi.h>
+
+// Function prototypes
+void initCommunication();
+void sendData(float voltage, float current, float rpm);
+
+// WiFi credentials
+const char* ssid = "your_SSID";
+const char* password = "your_PASSWORD";
+
+// Server settings
+const char* serverName = "http://yourserver.com/update";
+const char* apiKey = "your_API_KEY";
+
+void initCommunication() {
+  // Initialize WiFi
+  WiFi.begin(ssid, password);
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+  
+  Serial.println("Connected to WiFi");
+}
+
+void sendData(float voltage, float current, float rpm) {
+  if (WiFi.status() == WL_CONNECTED) {
+    WiFiClient client;
+    HTTPClient http;
+
+    // Prepare URL with data
+    String url = String(serverName) + "?api_key=" + apiKey + "&voltage=" + String(voltage) + "&current=" + String(current) + "&rpm=" + String(rpm);
+    
+    http.begin(client, url);
+    int httpResponseCode = http.GET();
+    
+    if (httpResponseCode > 0) {
+      String response = http.getString();
+      Serial.println(httpResponseCode);
+      Serial.println(response);
+    } else {
+      Serial.print("Error on sending GET: ");
+      Serial.println(httpResponseCode);
+    }
+    
+    http.end();
+  } else {
+    Serial.println("WiFi Disconnected");
+  }
+}
+
+#endif // COMMUNICATION_LIBRARY_H
